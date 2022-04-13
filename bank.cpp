04bc9738e493da2c972c4ad07fdc1d64115b374a
Mayu <80514810+changs14@users.noreply.github.com>
2022-04-12 23:15:07 -0700

    Add files via upload

---
+++ bank.cpp
@@ -0,0 +1,29 @@
+/*********************** Mr. Bean's Bank ***********************/
+/* This program will simulate a bank management system with the following features:
+    - Create an account
+    - Display account details
+    - Modify account 
+    - Delete account
+    - Deposit/Withdraw
+*/
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+//Message functions?
+
+//Functions for account management
+void createAccount();
+void displayAccount();
+void modifyAccount();
+void deleteAccount();
+void desposit(int);
+void withdraw();
+
+int main(){
+
+    
+}
