6859c379008bd8785bdaaf0e158fa9cc5fb71887
Mayu <80514810+changs14@users.noreply.github.com>
2022-04-12 23:56:56 -0700

    Add files via upload

--- bank.cpp
+++ bank.cpp
@@ -1,8 +1,8 @@
-/*********************** Mr. Bean's Bank ***********************/
-/* This program will simulate a bank management system with the following features:
-    - Create an account
+/*********************** Mr. Bean's Bank ***********************
+This program will simulate a bank management system with the following features:
+    - Create an account (chequing or savings)
     - Display account details
-    - Modify account 
+    - Modify account - Edit bank account nickname
     - Delete account
     - Deposit/Withdraw
 */
@@ -13,17 +13,104 @@
 
 using namespace std;
 
-//Message functions?
-
 //Functions for account management
 void createAccount();
 void displayAccount();
 void modifyAccount();
 void deleteAccount();
 void desposit(int);
-void withdraw();
+void withdraw(int);
+
+//Account class has class variables and functions
+class Account{
+    private:
+        int accountNumber;  //Randomized account number
+        char nickname[50];  //Account nickname
+        int deposit;        //Initial deposit for account
+        int accountType;    //Chequing or savings account
+
+    public:
+        //Enter functions
+};
 
 int main(){
+    char userAnswer; //User inputted menu option
 
+    //Display bank banner and welcome message
+    cout<<"\n******************************************************************"<<endl;
+    cout<<"************************* MR. BEAN'S BANK ************************"<<endl;
+    cout<<"******************************************************************"<<endl;
     
+    cout<<"\nHow may we help you today?"<<endl;
+
+    do{
+        //Display main menu
+        cout<<"1. CREATE NEW ACCOUNT"<<endl;
+        cout<<"2. DEPOSIT"<<endl;
+        cout<<"3. WITHDRAW"<<endl;
+        cout<<"4. CHECK ACCOUNT DETAILS"<<endl;
+        cout<<"5. MODIFY ACCOUNT"<<endl;
+        cout<<"6. DELETE ACCOUNT"<<endl;
+        cout<<"7. EXIT"<<endl;
+        cout<<endl;
+
+        cin>>userAnswer; //Get user input
+
+        switch(userAnswer){
+            case '1':
+                break;
+            case '2':
+                break;
+            case '3':
+                break;
+            case '4':
+                break;
+            case '5':
+                break;
+            case '6':
+                break;
+            case '7':
+                cout<<"\nThank you for banking with Mr. Bean's Bank. Have a nice day!"<<endl;
+                break;
+            default:
+                cout<<"\nInvalid menu option. Please pick from 1-7\n"<<endl;
+        }
+
+    }while(userAnswer!='7');
+
+
+
+    return 0;
+}
+
+//**************************************** FUNCTIONS ***********************************************
+
+/* Function: createAccount()
+   Parameters: None
+   createAccount will write and save details to an outfile: random account number, nickname, deposit, acc type
+*/
+void createAccount(){
+    Account newAccount;
+    
+
+}
+
+void displayAccount(){
+
+}
+
+void modifyAccount(){
+
+}
+
+void deleteAccount(){
+
+}
+
+void deposit(int amount){
+    //Change to value returning int?
+}
+
+void withdraw(int amount){
+    //Change to value returning int?
 }
