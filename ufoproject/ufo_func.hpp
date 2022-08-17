576faff9b41a4f439f3553243ff8b894349e27ff
Stephaney <80514810+changs14@users.noreply.github.com>
2022-08-16 20:41:16 -0700

    Add files via upload

---
+++ ufoproject/ufo_func.hpp
@@ -0,0 +1,10 @@
+#include <vector>
+#include <string>
+
+void display_misses(int misses);
+void display_status(std::vector<char> wrong, std::string ans);
+void greet();
+void end_game(std::string ans, std::string codeword);
+
+
+// Declare functions
